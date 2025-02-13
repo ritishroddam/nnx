c1b280c4cb32e96336e1937f5fc6be406edd8321
ritishroddam <ritishrajeshroddam@gmail.com>
2025-02-13 13:08:01 +0530

    changed from polling to web socket

--- Vehicle/static/js/vehicle.js
+++ Vehicle/static/js/vehicle.js
@@ -147,7 +147,7 @@ setupWebSocket();
 // }, 1000)};
 
 function setupWebSocket() {
-    socket = io("http://your-server-ip:5000");
+    socket = io("http://64.227.137.175:8000");
     socket.on("vehicle_update", (data) => updateVehicleMarker(data));
     socket.on("sos_alert", (data) => triggerSOS(data.imei, markers[data.imei]));
 }
