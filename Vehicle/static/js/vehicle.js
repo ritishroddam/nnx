39e970b8273d22254ca230483a0f9403bc8272bb
ritishroddam <ritishrajeshroddam@gmail.com>
2025-02-27 14:01:03 +0530

    chore: add console logging for time calculations in getCarIconBySpeed function

--- Vehicle/static/js/vehicle.js
+++ Vehicle/static/js/vehicle.js
@@ -480,12 +480,14 @@ function getCarIconBySpeed(speed, imei, date, time) {
   
   const now = new Date();
   const lastUpdateTime = new Date(`${date}T${time}`);
-
+  console.log(time);
+  console.log(lastUpdateTime);
   const timeDiff = now - lastUpdateTime;
   const hoursDiff = timeDiff / (1000 * 60 * 60);
-  
+  console.log(hoursDiff);
   if (hoursDiff >= 3) {
     iconUrl = "/vehicle/static/images/car_black.png";
+    console.log(iconUrl);
   }
 
   return iconUrl;
