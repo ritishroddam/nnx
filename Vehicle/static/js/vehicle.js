b136df173ce70628490fbd8108a0fc17c184c44e
ritishroddam <ritishrajeshroddam@gmail.com>
2025-03-03 11:20:03 +0530

    fix: streamline toggleView functionality by removing commented code and ensuring correct view switching

--- Vehicle/static/js/vehicle.js
+++ Vehicle/static/js/vehicle.js
@@ -987,26 +987,13 @@ function geocodeLatLng(latLng, callback) {
   }
 }
 
-// let toggleVIew = false;
-// function toggleView() {
-//   if (toggleVIew) {
-//     showMapView();
-//     toggleVIew = !toggleVIew;
-//   } else {
-//     showListView();
-//     toggleVIew = !toggleVIew;
-//   }
-// }
-
 document.querySelector(".toggle-slider").addEventListener("click", function () {
   this.classList.toggle("active");
 
   if (this.classList.contains("active")) {
-    // Call function when the element is active
-    showMapView();
-  } else {
-    // Call function when the element is not active
     showListView();
+  } else {
+    showMapView();
   }
 });
 
