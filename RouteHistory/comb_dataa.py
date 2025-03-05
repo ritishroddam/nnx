2524b860a9bec50bc0865239a64dca7906fa6dc5
ritishroddam <ritishrajeshroddam@gmail.com>
2025-03-05 17:40:01 +0530

    fix: update database connection from 'nnx' to 'CordonEV'

--- RouteHistory/comb_dataa.py
+++ RouteHistory/comb_dataa.py
@@ -9,7 +9,7 @@ app = Flask(__name__, template_folder="templates")
 CORS(app)
 
 client = MongoClient("mongodb+srv://doadmin:4T81NSqj572g3o9f@db-mongodb-blr1-27716-c2bd0cae.mongo.ondigitalocean.com/admin?tls=true&authSource=admin")
-db = client["nnx"]
+db = client["CordonEV"]
 data_collection = db["data"]
 atlanta_collection = db["atlanta"]
 
