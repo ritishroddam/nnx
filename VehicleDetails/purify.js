a70ff8f44a2fa55edf910c55bf58230c0b735fd3
ritishroddam <ritishrajeshroddam@gmail.com>
2025-03-07 16:49:22 +0530

    feat: add purify-css integration and optimize vehicle details CSS

---
+++ VehicleDetails/purify.js
@@ -0,0 +1,18 @@
+const purify = require("purify-css");
+
+// Reference of all HTML files from root directory
+let content = ["/templates/vehicleDetails.html"];
+
+// Reference of all CSS files from root directory
+let css = ["/static/css/vehicleDetails.css"];
+
+let files = {
+  // Write purified CSS into a file
+  output: "purified.css",
+  minified: true, // Minify boolean
+  info: true, // Output information
+};
+
+purify(content, css, files, function (purifiedAndMinifiedResult) {
+  console.log(purifiedAndMinifiedResult);
+});
