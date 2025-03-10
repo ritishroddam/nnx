7b181ea9f19f9f37b7ae1ac0f74d0c35f054a243
ritishroddam <ritishrajeshroddam@gmail.com>
2025-03-10 15:35:23 +0530

    feat: add CompanyDetailsEntry functionality with upload template, CSS, and JS for customer management

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from Dashboard.DashboardBackend import dashboard_bp
 from DeviceInvy.DeviceBackend import device_bp
 from RouteHistory.routeBackend import route_bp
 from CompanyDetails.companyBackend import company_bp
+from CompanyDetailsEntry.companyBackend import companyEntry_bp
 from SimInvy.SimBackend import sim_bp
 from SimInvyEntry.SimBackend import simEntry_bp
 from VehicleDetails.vehicleDetails import vehicleDetails_bp
@@ -25,6 +26,7 @@ app.register_blueprint(dashboard_bp, url_prefix='/dashboard')
 app.register_blueprint(device_bp, url_prefix='/deviceInvy')
 app.register_blueprint(route_bp, url_prefix='/routeHistory')
 app.register_blueprint(company_bp, url_prefix='/companyDetails')
+app.register_blueprint(companyEntry_bp, url_prefix='/companyDetailsEntry')
 app.register_blueprint(sim_bp, url_prefix='/simInvy')
 app.register_blueprint(simEntry_bp, url_prefix='/simInvyEntry')
 app.register_blueprint(vehicleDetails_bp, url_prefix='/vehicleDetails')
@@ -41,6 +43,10 @@ def vehicleDetailsEntry():
 @app.route('/SimInvyEntry')
 def simInvyEntry():
     return render_template('simEntry.html')
+
+@app.route('/CompanyDetailsEntry')
+def companyDetailsEntry():
+    return render_template('companyEntry.html')
 
 @app.route("/default") 
 def default(): 
