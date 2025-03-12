cda3c2865cbbee832fb0a6e64107ce798bbdf0c9
ritishroddam <ritishrajeshroddam@gmail.com>
2025-03-12 17:52:17 +0530

    ``` chore: remove empty code changes for cleanup ```

--- Dashboard/DashboardBackend.py
+++ Dashboard/DashboardBackend.py
@@ -175,10 +175,8 @@ def get_vehicle_distances():
         today_str = datetime.now().strftime('%d%m%y')  # Format: DDMMYY
 
         # Fetch vehicle IMEI mappings
-        vehicle_map = {
-            v.get("imei", "UNKNOWN"): v.get("LicensePlateNumber", "UNKNOWN") 
-            for v in vehicle_inventory.find({}, {"LicensePlateNumber": 1, "imei": 1, "_id": 0})
-        }
+        vehicle_map = vehicle_inventory.find({}, {"imei": 1, "LicensePlateNumber": 1, "_id": 0})
+        
 
         print("Vehicle Map:", vehicle_map)  # Debugging log
 
