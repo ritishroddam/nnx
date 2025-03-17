6cd4f2b4fa64ae0f50f49093026be306f9c70952
ritishroddam <ritishrajeshroddam@gmail.com>
2025-03-17 18:12:49 +0530

    refactor: improve latitude and longitude precision in updateVehicleData by formatting to six decimal places

--- Vehicle/static/js/vehicle.js
+++ Vehicle/static/js/vehicle.js
@@ -512,7 +512,10 @@ function sanitizeIMEI(imei) {
 function updateVehicleData(vehicle) {
   const imei = sanitizeIMEI(vehicle.imei);
   const coords = parseCoordinates(vehicle.latitude, vehicle.longitude);
-  const latLng = new google.maps.LatLng(coords.lat, coords.lon);
+  const latLng = new google.maps.LatLng(
+    coords.lat.toFixed(6),
+    coords.lon.toFixed(6)
+  );
   const iconUrl = getCarIconBySpeed(
     vehicle.speed,
     imei,
