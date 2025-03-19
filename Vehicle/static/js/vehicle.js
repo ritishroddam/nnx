09b6db97e37c20f1dd3be1aca2f3a1ebdf2e4099
ritishroddam <ritishrajeshroddam@gmail.com>
2025-03-19 14:19:06 +0530

    fix: adjust panToWithOffset function parameters for correct map panning

--- Vehicle/static/js/vehicle.js
+++ Vehicle/static/js/vehicle.js
@@ -903,7 +903,7 @@ function updateAdvancedMarker(marker, latLng, iconUrl, rotation) {
   addMarkerClickListener(marker, latLng, marker.device, coords);
 }
 
-function panToWithOffset(latLng, offsetX = 50, offsetY = 0) {
+function panToWithOffset(latLng, offsetX = -50, offsetY = 0) {
   // Get the current map projection
   const scale = Math.pow(2, map.getZoom()); // Scale based on zoom level
   const worldCoordinateCenter = map.getProjection().fromLatLngToPoint(latLng);
@@ -938,7 +938,7 @@ function addHoverListenersToCardsAndMarkers() {
         );
 
         map.setZoom(20);
-        map.panTo(panToWithOffset(latLng, 50, 0));
+        map.panTo(panToWithOffset(latLng, -50, 0));
 
         // Open the info window for the marker
         const coords = {
