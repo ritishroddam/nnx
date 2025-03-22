1eb1f6e0e90a42b23486acb11dee7d7b61983b84
ritishroddam <ritishrajeshroddam@gmail.com>
2025-03-22 14:42:29 +0530

    feat: register speed report blueprint in allReports module

--- Reports/allReports.py
+++ Reports/allReports.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, Blueprint
+from Reports.SpeedReport.speed import speed_bp
 
 reports_bp = Blueprint('Reports', __name__, static_folder='static', template_folder='templates')
 
+reports_bp.register_blueprint(speed_bp, url_prefix='/speed')
 
 @reports_bp.route('/')
 def index():
