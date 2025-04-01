5f72a0b51a060a7ab92cd3b9c2dfcfaf8c785ea8
ritishroddam <ritishrajeshroddam@gmail.com>
2025-04-01 17:11:19 +0530

    fix: update MongoDB connection to use MongoClient for improved database access

--- app/t.py
+++ app/t.py
@@ -292,7 +292,8 @@ import threading
 import time
 
 # MongoDB collections
-from app import db
+mongo_client = MongoClient("mongodb+srv://doadmin:4T81NSqj572g3o9f@db-mongodb-blr1-27716-c2bd0cae.mongo.ondigitalocean.com/admin?tls=true&authSource=admin")
+db = mongo_client["nnx"]
 
 collection = db['atlanta']
 sos_logs_collection = db['sos_logs']
