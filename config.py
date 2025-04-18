75df9132d2dab4689d86ae5e94f68671f5ca9fe3
ritishroddam <ritishrajeshroddam@gmail.com>
2025-04-18 13:49:41 +0530

    fix: update JWT access token expiration to 1 hour for improved security

--- config.py
+++ config.py
@@ -2,11 +2,10 @@ import os
 from datetime import timedelta
 
 class Config:
-    GMAPS_API_KEY = os.environ.get('GMAPS_API_KEY') or 'AIzaSyDEFA1-1dlca1C2BbUNKpQEf-icQAJAfX0'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt-secret-key-here'
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=1)
-    JWT_REFRESH_TOKEN_EXPIRES = timedelta(minutes=5) 
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=2) 
     JWT_TOKEN_LOCATION = ['cookies']
     JWT_COOKIE_SECURE = True
     JWT_COOKIE_CSRF_PROTECT = True
@@ -17,6 +16,7 @@ class Config:
     JWT_CSRF_IN_COOKIES = True
     JWT_REFRESH_CSRF_PROTECT = True    
     
+    GMAPS_API_KEY = os.environ.get('GMAPS_API_KEY') or 'AIzaSyDEFA1-1dlca1C2BbUNKpQEf-icQAJAfX0'
     MONGO_URI = "mongodb+srv://doadmin:4T81NSqj572g3o9f@db-mongodb-blr1-27716-c2bd0cae.mongo.ondigitalocean.com/admin?tls=true&authSource=admin"
     
     @staticmethod
