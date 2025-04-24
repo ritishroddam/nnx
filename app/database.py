bb55de312e801cb990329f4622c43b5739230680
ritishroddam <ritishrajeshroddam@gmail.com>
2025-04-24 13:59:58 +0530

    fix: enable timezone awareness for MongoDB connection in database.py

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 from config import config
 
-mongo_client = MongoClient(config['default'].MONGO_URI)
+mongo_client = MongoClient(config['default'].MONGO_URI, tz_aware=True)
 db = mongo_client["nnx"]
