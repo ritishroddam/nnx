5bb279ec4246ad7eb1454e3f5a7ac7fdcf405956
ritishroddam <ritishrajeshroddam@gmail.com>
2025-04-24 14:00:24 +0530

    fix: clean up code structure in map_server.py

--- app/map_server.py
+++ app/map_server.py
@@ -18,7 +18,7 @@ import time
 from pymongo import MongoClient
 import ssl
 
-mongo_client = MongoClient("mongodb+srv://doadmin:4T81NSqj572g3o9f@db-mongodb-blr1-27716-c2bd0cae.mongo.ondigitalocean.com/admin?tls=true&authSource=admin")
+mongo_client = MongoClient("mongodb+srv://doadmin:4T81NSqj572g3o9f@db-mongodb-blr1-27716-c2bd0cae.mongo.ondigitalocean.com/admin?tls=true&authSource=admin", tz_aware=True)
 db = mongo_client["nnx"]
 
 app = Flask(__name__)
