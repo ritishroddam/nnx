c3676940702ac802379c5d2f39dd5807e7ba223a
ritishroddam <ritishrajeshroddam@gmail.com>
2025-05-14 18:48:26 +0530

    fix: improve socket connection handling for vehicle data updates

--- app/RouteHistory/static/js/routePlayBack.js
+++ app/RouteHistory/static/js/routePlayBack.js
@@ -63,7 +63,7 @@ async function plotPolyLineLiveMap(liveData) {
     const status = recentData.status;
     const statusTime = recentData.status_time;
     const address = recentData.address;
-    const speed = null;
+    let speed = null;
 
     if (status === "Moving") {
       speed = `<p><strong>Speed:</strong> ${recentData.speed}</p>`;
