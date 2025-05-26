35b7bea91ae27a0c0a9d8d184f5d2558e0bb20d0
ritishroddam <ritishrajeshroddam@gmail.com>
2025-05-26 15:47:01 +0530

    fix: remove unused token variable and clean up initialization logging in share_location.js

--- app/Vehicle/static/js/share_location.js
+++ app/Vehicle/static/js/share_location.js
@@ -1,7 +1,5 @@
 window.onload =  initMap;
 
-const token = window.shareToken; 
-
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
@@ -9,6 +7,8 @@ async function initMap() {
   const lng = window.vehicleLng;
 
   const latLng = new google.maps.LatLng(lat, lng);
+
+  console.log("Initializing map at:", latLng, lat, lng);
 
   const map = new Map(document.getElementById("map"), {
     mapId: "e426c1ad17485d79",
@@ -26,17 +26,4 @@ async function initMap() {
     const url = `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`;
     window.open(url, "_blank");
   };
-
-  // Connect to WebSocket
-  const socket = io(); // Assumes Socket.IO JS is loaded
-
-  socket.emit('join', { token });
-
-  socket.on('location_update', function (data) {
-    if (data.latitude && data.longitude) {
-      const newLatLng = { lat: data.latitude, lng: data.longitude };
-      marker.setPosition(newLatLng);
-      map.setCenter(newLatLng);
-    }
-  });
-};
+}
