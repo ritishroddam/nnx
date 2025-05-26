2054a736190a514908d767916d58e7466e0da232
ritishroddam <ritishrajeshroddam@gmail.com>
2025-05-26 16:59:08 +0530

    fix: update mapId for correct map initialization

--- app/Vehicle/static/js/share_location.js
+++ app/Vehicle/static/js/share_location.js
@@ -9,7 +9,7 @@ async function initMap() {
   const latLng = new google.maps.LatLng(lat, lng);
 
   const map = new Map(document.getElementById("map"), {
-    mapId: "e426c1ad17485d79",
+    mapId: "dc4a8996aab2cac9",
     center: latLng,
     zoom: 16,
   });
