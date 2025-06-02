c25a172ac54f56725e7ea27b915202fc606bc0f3
ritishroddam <ritishrajeshroddam@gmail.com>
2025-06-02 17:18:58 +0530

    fix: remove unused imports in tasks.py for cleaner code

--- app/Vehicle/tasks.py
+++ app/Vehicle/tasks.py
@@ -1,10 +1,10 @@
 from app import create_app
 from app.celery_worker import make_celery
-from app.Vehicle.VehicleBackend import getVehicleStatus
 
 flask_app = create_app()
 celery = make_celery(flask_app)
 
 @celery.task()
 def get_vehicle_status_task(imei_list):
+    from app.Vehicle.VehicleBackend import getVehicleStatus
     return getVehicleStatus(imei_list)
