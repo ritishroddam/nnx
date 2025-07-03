ec16eb272e391107f636e6f644668f4df1e558cc
Ritish <ritishrajeshroddam@gmail.com>
2025-07-03 18:32:44 +0530

    Refactor error handling in company data retrieval for improved robustness in mapZoomIn.py

--- app/MapZoomIn/mapZoomIn.py
+++ app/MapZoomIn/mapZoomIn.py
@@ -27,6 +27,6 @@ def home():
             companyLatLng = company_collection.find_one({'_id': ObjectId(companyId)}, {'_id': 0,'lat': 1, 'lng': 1})
         except Exception as e:
             print(f"[DEBUG] Error fetching company data: {e}")
-            companyLatLng = {'lat': "13.0142181596867", 'lng': "77.64852894386185"}
+            companyLatLng = {'lat': "15.34776", 'lng': "75.13378"}
             
     return render_template('mapZoomIn.html', companyLatLng=companyLatLng, companyName=company)
