94661a121477e15853fd97dddc4a1f3e019223d0
shreyas3500 <117635124+shreyas3500@users.noreply.github.com>
2025-07-07 14:29:13 +0530

    vehicle icon updates

--- app/Vehicle/static/js/vehicle.js
+++ app/Vehicle/static/js/vehicle.js
@@ -940,6 +940,7 @@ function updateMap() {
     ) {
       const latLng = parseCoordinates(device.latitude, device.longitude);
       const iconUrl = getCarIconBySpeed(
+        device.VehicleType,
         device.speed,
         imei,
         device.date,
@@ -1090,16 +1091,62 @@ function convertSpeedToKmh(speedkmh) {
   return parseFloat(speedkmh);
 }
 
-function getCarIconUrlBySpeed(speedInKmh) {
-  if (speedInKmh === 0) {
-    return "/static/images/car_yellow.png";
-  } else if (speedInKmh > 0 && speedInKmh <= 40) {
-    return "/static/images/car_green.png";
-  } else if (speedInKmh > 40 && speedInKmh <= 60) {
-    return "/static/images/car_blue.png";
-  } else {
-    return "/static/images/car_red.png";
+// function getCarIconUrlBySpeed(speedInKmh) {
+//   if (speedInKmh === 0) {
+//     return "/static/images/car_yellow.png";
+//   } else if (speedInKmh > 0 && speedInKmh <= 40) {
+//     return "/static/images/car_green.png";
+//   } else if (speedInKmh > 40 && speedInKmh <= 60) {
+//     return "/static/images/car_blue.png";
+//   } else {
+//     return "/static/images/car_red.png";
+//   }
+// }
+
+function getVehicleIconUrlByTypeAndSpeed(vehicleType, speedInKmh) {
+  // Determine base icon based on vehicle type
+  let baseIcon;
+  switch(vehicleType.toLowerCase()) {
+    case 'bike':
+      baseIcon = 'bike';
+      break;
+    case 'bus':
+      baseIcon = 'bus';
+      break;
+    case 'truck':
+      baseIcon = 'truck';
+      break;
+    case 'hatchback':
+    case 'sedan':
+    case 'suv':
+    case 'van':
+    default:
+      baseIcon = 'car';
   }
+
+  // Determine color based on speed
+  let color;
+  if (speedInKmh === 0) {
+    color = 'yellow';
+  } else if (speedInKmh > 0 && speedInKmh <= 40) {
+    color = 'green';
+  } else if (speedInKmh > 40 && speedInKmh <= 60) {
+    color = 'blue';
+  } else {
+    color = 'red';
+  }
+
+  // Check if offline (more than 24 hours)
+  const now = new Date();
+  const lastUpdateTime = convertToDate(device.date, device.time);
+  const timeDiff = now - lastUpdateTime;
+  const dayDiff = timeDiff / (1000 * 60 * 60 * 24);
+
+  if (dayDiff >= 1) {
+    color = 'black';
+  }
+
+  return `/static/images/${baseIcon}_${color}.png`;
 }
 
 function convertToDate(ddmmyyyy, hhmmss) {
@@ -1118,21 +1165,26 @@ function convertToDate(ddmmyyyy, hhmmss) {
   return dateObj;
 }
 
-function getCarIconBySpeed(speed, imei, date, time) {
+// function getCarIconBySpeed(speed, imei, date, time) {
+//   const speedInKmh = convertSpeedToKmh(speed);
+//   let iconUrl = getCarIconUrlBySpeed(speedInKmh);
+
+//   const now = new Date();
+//   const lastUpdateTime = convertToDate(date, time);
+
+//   const timeDiff = now - lastUpdateTime;
+//   const dayDiff = timeDiff / (1000 * 60 * 60 * 24);
+
+//   if (dayDiff >= 1) {
+//     iconUrl = "/static/images/car_black.png";
+//   }
+
+//   return iconUrl;
+// }
+
+function getVehicleIconByTypeAndSpeed(vehicleType, speed, imei, date, time) {
   const speedInKmh = convertSpeedToKmh(speed);
-  let iconUrl = getCarIconUrlBySpeed(speedInKmh);
-
-  const now = new Date();
-  const lastUpdateTime = convertToDate(date, time);
-
-  const timeDiff = now - lastUpdateTime;
-  const dayDiff = timeDiff / (1000 * 60 * 60 * 24);
-
-  if (dayDiff >= 1) {
-    iconUrl = "/static/images/car_black.png";
-  }
-
-  return iconUrl;
+  return getVehicleIconUrlByTypeAndSpeed(vehicleType, speedInKmh);
 }
 
 function checkForDataTimeout(imei) {
@@ -1173,6 +1225,7 @@ function updateVehicleData(vehicle) {
   const imei = vehicle.imei;
   const latLng = parseCoordinates(vehicle.latitude, vehicle.longitude); 
   const iconUrl = getCarIconBySpeed(
+    vehicle.VehicleType,
     vehicle.speed,
     imei,
     vehicle.date,
@@ -1218,14 +1271,12 @@ function removeSOS(imei) {
     marker.content.classList.remove("vehicle-blink");
   }
 
-  // Update the vehicle data to clear SOS
   const vehicle = vehicleData.get(imei);
   if (vehicle) {
     vehicle.sos = "0";
     vehicleData.set(imei, vehicle);
   }
 
-  // Re-render cards to update positions
   const vehiclesArray = Array.from(vehicleData.values());
   renderVehicleCards(vehiclesArray);
 }
@@ -1395,7 +1446,6 @@ async function populateVehicleTable() {
       vehicle.date,
       vehicle.time
     );
-
     row.insertCell(3).innerText = `${vehicle.address || "Location unknown"}`;
     row.insertCell(4).innerText = latitude ? latitude.toFixed(4) : "N/A";
     row.insertCell(5).innerText = longitude ? longitude.toFixed(4) : "N/A";
