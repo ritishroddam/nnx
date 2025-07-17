9f17b9ee95777c18f7d69df0b27d8d2b2be79b58
shreyas3500 <117635124+shreyas3500@users.noreply.github.com>
2025-07-17 10:34:23 +0530

    update

--- app/Vehicle/static/js/vehicle.js
+++ app/Vehicle/static/js/vehicle.js
@@ -1027,7 +1027,7 @@ function updateMap() {
   filterVehicles();
 }
 
-function animateMarker(marker, newPosition, duration = 9000) {
+function animateMarker(marker, newPosition, duration = 10000) {
   let startPosition = new google.maps.LatLng(
     marker.position.lat,
     marker.position.lng
