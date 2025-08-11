ad0374b049a6016b840a24a76f1f8f725087ecf4
shreyas3500 <117635124+shreyas3500@users.noreply.github.com>
2025-08-11 11:10:08 +0530

    style: increase map zoom level for enhanced detail visibility

--- app/Dashboard/static/js/dashboard.js
+++ app/Dashboard/static/js/dashboard.js
@@ -812,7 +812,7 @@ async function fallbackToDefaultLocation() {
     const mapId = darkMode ? "e426c1ad17485d79" : "dc4a8996aab2cac9";
     map = new Map(document.getElementById("map"), {
       center: defaultLocation,
-      zoom: 13,
+      zoom: 15,
       disableDefaultUI: true,
       mapId: mapId,
     });
