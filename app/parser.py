e8511e3385e0b0a0fb8b3f348bcb13fc3515b5b8
Ritish <ritishrajeshroddam@gmail.com>
2025-08-26 18:23:04 +0530

    fix: remove duplicate keys in json_data dictionary in atlantaAis140ToFront function

--- app/parser.py
+++ app/parser.py
@@ -11,39 +11,35 @@ def _convert_date_time_emit(date):
     return ist.strftime("%d%m%y"), ist.strftime("%H%M%S")
 
 def atlantaAis140ToFront(parsedData):
-    try:
-        date, time = _convert_date_time_emit(parsedData.get("timestamp"))
+    date, time = _convert_date_time_emit(parsedData.get("timestamp"))
 
-        address = geocodeInternal(parsedData.get("gps", {}).get("lat"), parsedData.get("gps", {}).get("lon"))
-        json_data = {
-            "imei": parsedData.get("imei"),
-            "speed": parsedData.get("telemetry", {}).get("speed"),
-            "latitude": parsedData.get("gps", {}).get("lat"),
-            "dir1": parsedData.get("gps", {}).get("latDir"),
-            "longitude": parsedData.get("gps", {}).get("lon"),
-            "dir2": parsedData.get("gps", {}).get("lonDir"),
-            "date": date,
-            "time": time,
-            "course": parsedData.get("gps", {}).get("heading"),
-            "address": address,
-            "ignition": parsedData.get("telemetry", {}).get("ignition"),
-            "gsm_sig": parsedData.get("network", {}).get("gsmSignal"),
-            "sos": parsedData.get("telemetry", {}).get("emergencyStatus"),
-            "odometer": parsedData.get("telemetry", {}).get("odometer"),
-            "main_power": parsedData.get("telemetry", {}).get("mainPower"),
-            "harsh_speed": "0" if parsedData.get("packet").get("id") != "14" else "1",
-            "harsh_break": "0" if parsedData.get("packet").get("id") != "13" else "1",
-            "adc_voltage": parsedData.get("telemetry", {}).get("mainBatteryVoltage"),
-            "odometer": parsedData.get("telemetry", {}).get("odometer"),
-            "internal_bat": parsedData.get("telemetry", {}).get("internalBatteryVoltage"),
-            "gsm_sig": parsedData.get("network", {}).get("gsmSignal"),
-            "mobCountryCode": parsedData.get("network", {}).get("mcc"),
-            "mobNetworkCode": parsedData.get("network", {}).get("mnc"),
-            "localAreaCode": parsedData.get("network", {}).get("lac"),
-            "cellid": parsedData.get("network", {}).get("cellId"),
-            "date_time": parsedData.get("timestamp"),
-        }
-        return json_data
-    except Exception as e:
-        print(f"Error in atlantaAis140ToFront: {e}")
-        return {}
+    address = geocodeInternal(parsedData.get("gps", {}).get("lat"), parsedData.get("gps", {}).get("lon"))
+    json_data = {
+        "imei": parsedData.get("imei"),
+        "speed": parsedData.get("telemetry", {}).get("speed"),
+        "latitude": parsedData.get("gps", {}).get("lat"),
+        "dir1": parsedData.get("gps", {}).get("latDir"),
+        "longitude": parsedData.get("gps", {}).get("lon"),
+        "dir2": parsedData.get("gps", {}).get("lonDir"),
+        "date": date,
+        "time": time,
+        "course": parsedData.get("gps", {}).get("heading"),
+        "address": address,
+        "ignition": parsedData.get("telemetry", {}).get("ignition"),
+        "gsm_sig": parsedData.get("network", {}).get("gsmSignal"),
+        "sos": parsedData.get("telemetry", {}).get("emergencyStatus"),
+        "odometer": parsedData.get("telemetry", {}).get("odometer"),
+        "main_power": parsedData.get("telemetry", {}).get("mainPower"),
+        "harsh_speed": "0" if parsedData.get("packet").get("id") != "14" else "1",
+        "harsh_break": "0" if parsedData.get("packet").get("id") != "13" else "1",
+        "adc_voltage": parsedData.get("telemetry", {}).get("mainBatteryVoltage"),
+        "odometer": parsedData.get("telemetry", {}).get("odometer"),
+        "internal_bat": parsedData.get("telemetry", {}).get("internalBatteryVoltage"),
+        "gsm_sig": parsedData.get("network", {}).get("gsmSignal"),
+        "mobCountryCode": parsedData.get("network", {}).get("mcc"),
+        "mobNetworkCode": parsedData.get("network", {}).get("mnc"),
+        "localAreaCode": parsedData.get("network", {}).get("lac"),
+        "cellid": parsedData.get("network", {}).get("cellId"),
+        "date_time": parsedData.get("timestamp"),
+    }
+    return json_data
