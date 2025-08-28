d58656cf0a3c39f5e0387487b15ad4162790ca45
Ritish <ritishrajeshroddam@gmail.com>
2025-08-28 12:28:08 +0530

    fix: update debug print statement for specific IMEI status in build_vehicle_data function

--- app/Vehicle/VehicleBackend.py
+++ app/Vehicle/VehicleBackend.py
@@ -323,7 +323,7 @@ def build_vehicle_data(inventory_data, distances, stoppage_times, statuses, imei
         if imei not in missingImeis:
             status_item = status_lookup.get(imei, {})
             if imei == "863070047070049":
-                print(status_item.get('status'), imei)
+                print(status_item, imei)
             vehicle['status'] = status_item.get('status', 'unnown')
             vehicle['status_time_str'] = status_item.get('status_time_str', '0 seconds')
             vehicle['status_time_delta'] = status_item.get('status_time_delta', 0)
