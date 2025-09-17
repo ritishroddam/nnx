82aa62e928d0cac4af4b4a4e805163d773164993
Ritish <ritishrajeshroddam@gmail.com>
2025-09-17 18:02:33 +0530

    Refactor allReports.py for improved organization and clarity

--- app/Reports/allReports.py
+++ app/Reports/allReports.py
@@ -348,6 +348,7 @@ def process_speed_report(imeis, vehicles, date_filter):
                 data.append(cursor)
         print(data)
         df = pd.DataFrame(data)
+        print(df)
         return df
     except Exception as e:
         print(e)
