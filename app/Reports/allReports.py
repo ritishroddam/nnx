72e2c6b8ea7d373ca8e552acd45df8c3778256e9
Ritish R Roddam <ritishrajeshroddam@gmail.com>
2025-09-19 13:27:49 +0530

    Refactor FIELD_COLLECTION_MAP for improved readability and maintainability

--- app/Reports/allReports.py
+++ app/Reports/allReports.py
@@ -285,13 +285,6 @@ def process_travel_path_report(df):
             df['distance'] = df['distance'].cumsum()
             df['distance'] = pd.to_numeric(df['distance'], errors='coerce').round(3)
             
-            # df['odometer'] = pd.to_numeric(df['odometer'], errors='coerce')
-            # total_distance = df['odometer'].iloc[0] - df['odometer'].iloc[-2]
-            # summary = [""] * len(df.columns)
-            # summary[0] = "Total Distance"
-            # summary[1] = round(total_distance, 3) if pd.notnull(total_distance) else ""
-            # summary_row = pd.DataFrame([summary], columns=df.columns)
-            # df = pd.concat([df, summary_row], ignore_index=True)
         return df
     except:
         return   
