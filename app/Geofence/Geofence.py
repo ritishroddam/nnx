df90b46313abe5bdc4164f0f6860d882c5973cd5
shreyas3500 <117635124+shreyas3500@users.noreply.github.com>
2025-09-30 11:51:57 +0530

    updates

--- app/Geofence/Geofence.py
+++ app/Geofence/Geofence.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, request, jsonify
-from flask_login import login_required
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
 
 geofence_bp = Blueprint('Geofence', __name__, static_folder='static', template_folder='templates')
-@geofence_bp.route('/geofence', methods=['GET'])
-@login_required
+
+@geofence_bp.route('/page')
+@jwt_required()
 def page():
     return render_template('geofence.html')
