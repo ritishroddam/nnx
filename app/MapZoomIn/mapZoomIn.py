562dff4b45877afdea58fcb7bebe0d4d9aa6faac
Ritish R Roddam <ritishrajeshroddam@gmail.com>
2025-11-22 11:11:18 +0530

    Update company location notification message for clarity

--- app/MapZoomIn/mapZoomIn.py
+++ app/MapZoomIn/mapZoomIn.py
@@ -32,6 +32,6 @@ def home():
         
         if companyLatLng.get('lat', '') in (None, "",) or companyLatLng.get('lng', '') in (None, "",):
             companyLatLng = {'lat': "13.01387976", 'lng': "77.6486188"}
-            company += '/n Please contact Cordon Telematics to update company location, to get accurate company location'
+            company += "\nPlease contact Cordon Telematics to update company location,\nTo reflect your company's location on map"
             
     return render_template('mapZoomIn.html', companyLatLng=companyLatLng, companyName=company)
