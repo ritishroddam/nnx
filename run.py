a252b5f087a050177b9002279ddc292ecb7c6386
Ritish R Roddam <ritishrajeshroddam@gmail.com>
2025-12-10 15:21:38 +0530

    refactor: replace eventlet server with socketio.run for improved handling

--- run.py
+++ run.py
@@ -1,15 +1,32 @@
-import socket
-from app import create_app, socketio
-import os
+# import socket
+# from app import create_app, socketio
+# import os
+# import eventlet
+# import ssl
+
+# app = create_app()
+
+# if __name__ == '__main__':
+    
+#     eventlet.wsgi.server(
+#         eventlet.listen(('0.0.0.0', 5000)), 
+#         app,
+#         socket_timeout = 600
+#     )
+
 import eventlet
-import ssl
+eventlet.monkey_patch()
+
+from app import create_app, socketio
 
 app = create_app()
 
-if __name__ == '__main__':
-    
-    eventlet.wsgi.server(
-        eventlet.listen(('0.0.0.0', 5000)), 
-        app,
-        socket_timeout = 600
-    )
+if __name__ == "__main__":
+    if __name__ == "__main__":
+        socketio.run(
+            app,
+            host="0.0.0.0",
+            port=5000,
+            allow_unsafe_werkzeug=False,
+            socket_timeout=600,  # seconds
+        )
