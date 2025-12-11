d1a6d0823ed39e3f40e3e0f5ee129ebd3b980d1e
Ritish R Roddam <ritishrajeshroddam@gmail.com>
2025-12-11 13:42:36 +0530

    feat: update center and legend label colors for improved visibility in dark mode

--- app/Dashboard/static/js/dashboard.js
+++ app/Dashboard/static/js/dashboard.js
@@ -515,7 +515,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       let gradient1, gradient2, gradient3, gradient4;
       let hover1, hover2, hover3, hover4;
-      let centerColor = isDarkMode ? "red" : "#green";
+      let centerColor = isDarkMode ? "#ccc" : "black";
 
       if (isDarkMode) {
         gradient1 = makeGradient("#1B5BBE", "#4A90E2");
@@ -569,7 +569,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             legend: {
               position: "top",
               labels: {
-                color: isDarkMode ? "black" : "#ccc",
+                color: isDarkMode ? "#ccc" : "black",
                 font: {
                   size: 14,
                   weight: "bold",
