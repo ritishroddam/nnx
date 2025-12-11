08d9814f736b51c2efa5d44dcdae8eb43afd7b10
Ritish R Roddam <ritishrajeshroddam@gmail.com>
2025-12-11 14:53:40 +0530

    fix: remove debug print statement from atlanta_pie_data function

--- app/Dashboard/DashboardBackend.py
+++ app/Dashboard/DashboardBackend.py
@@ -316,7 +316,6 @@ def atlanta_pie_data():
                 
             speed = float(latest_data.get("speed", 0))
             ignition = latest_data.get("ignition", "0")
-            print(speed, ignition)
             if ignition == "1" or ignition == 1:
                 if speed > 0:
                     moving_vehicles += 1
