c460c3dbf5b68adb15c0e7c9d9f2e7912d580cae
shreyas3500 <117635124+shreyas3500@users.noreply.github.com>
2025-12-18 12:31:54 +0530

    updates

--- app/Vehicle/static/js/vehicle.js
+++ app/Vehicle/static/js/vehicle.js
@@ -2123,7 +2123,7 @@ function updateMap() {
 //   requestAnimationFrame(moveMarker);
 // }
 
-function animateMarker(marker, newPosition, duration = 10000) {
+function animateMarker(marker, newPosition, duration = 15000) {
   // New: ease function
   function easeInOutCubic(t) {
     return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
